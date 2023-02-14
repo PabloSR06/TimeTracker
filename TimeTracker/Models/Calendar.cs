5b85ce06e90065bac39d3c7869dcc92619e68a2e
Pablo <47283527+PabloSR06@users.noreply.github.com>
2023-02-14 21:07:07 +0000

    WIP: Some css in calendar min

--- TimeTracker/Models/Calendar.cs
+++ TimeTracker/Models/Calendar.cs
@@ -11,14 +11,32 @@ namespace TimeTracker.Models
 
         public int Day;
 
+        public CalendarInput()
+        {
+            var date = DateTime.Today;
+            Year = date.Year;
+            Month = date.Month;
+            Day = date.Day;
+            WeekNumber = (int)new DateTime(Year, Month, 1).DayOfWeek;
+        }
+
+        public CalendarInput(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            WeekNumber = (int)new DateTime(Year, Month, 1).DayOfWeek;
+        }
+
         public DateOnly ToDate()
         {
             if (Day == 0)
             {
-                Day= 1;
+                Day= DateTime.Today.Day;
             }
             return new DateOnly(Year, Month, Day);
 
         }
+
     }
 }
