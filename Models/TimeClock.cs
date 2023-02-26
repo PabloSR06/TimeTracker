866d2868c08639de76e4e64863bad68e4e8ef4dd
Pablo <47283527+PabloSR06@users.noreply.github.com>
2023-02-26 14:11:43 +0000

    WIP: Change folders

---
+++ Models/TimeClock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TimeTracker.Models
+{
+    public class TimeClock
+    {
+        public int Id { get; set; }
+        public int user_id { get; set; }
+        public TimeSpan? start_time { get; set; }
+        public TimeSpan? finish_time { get; set; }
+        public Boolean isFinish { get; set; }
+        public DateTime date { get; set; }
+    }
+
+    public class ClockInModel
+    {
+        public int user_id { get; set; }
+        public TimeSpan start_time { get; set; }
+        public DateOnly date { get; set; }
+
+    }
+
+    public class ClockOutModel
+    {
+        public int user_id { get; set; }
+        public TimeSpan finish_time { get; set; }
+        public DateOnly date { get; set; }
+
+    }
+
+    public class CheckInTime
+    {
+        public TimeClock? time_table { get; set; }
+
+        public List<ClockHistoryMin> clockHistories { get; set; }
+        public Boolean isOpen { get; set; }
+    }
+}
