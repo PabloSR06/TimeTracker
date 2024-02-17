0e3d8a0f1775827a4b8e5090452d8813dc192f34
Pablo Suarez <47283527+PabloSR06@users.noreply.github.com>
2024-02-17 23:24:02 +0000

    feat: login checks

--- time-tracker/src/componets/login/login.tsx
+++ time-tracker/src/componets/login/login.tsx
@@ -1,4 +1,4 @@
-import {BaseSyntheticEvent, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {ApiLogInUserData, checkTokenValidity} from "../types/config.ts";
 import {useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
@@ -6,47 +6,20 @@ import {logIn} from "../slice/userSlice.tsx";
 import {RootState} from "../slice/store.tsx";
 import styles from './login.module.css';
 import {useTranslation} from "react-i18next";
+import {useForm} from "react-hook-form";
 
 export const Login = () => {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const [selectedEmail, setSelectedEmail] = useState("");
-    const [selectedPassword, setSelectedPassword] = useState("");
-
     const token = useSelector((state: RootState) => state.user.userToken);
-
     const [isLoading, setIsLoading] = useState(false);
 
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const [formData, setFormData] = useState<ApiLogInUserData>({email: '', password: ''});
 
-    const handleEmail = (e: BaseSyntheticEvent) => {
-        setSelectedEmail(e.target.value);
-    };
-    const handlePassword = (e: BaseSyntheticEvent) => {
-        setSelectedPassword(e.target.value);
-    };
-
-    
-    const handleSend = async () => {
-        setIsLoading(true);
-        const data: ApiLogInUserData = {
-            email: selectedEmail,
-            password: selectedPassword
-        };
-        await logIn(dispatch, data).then(() => {
-            console.log('Token: ' + token)
-            if (token) {
-                setSelectedEmail("");
-                setSelectedPassword("");
-
-                navigate('/', {replace: true, state: {isLoggedIn: true}});
-            } else {
-                console.log('No token');
-            }
-            setIsLoading(false);
-        });
-    }
+    const goToForgotPassword = () => navigate('/password/forgot', {replace: true});
 
     useEffect(() => {
         if (checkTokenValidity()) {
@@ -54,27 +27,79 @@ export const Login = () => {
         }
     }, [token]);
 
-    const goToForgotPassword = () => navigate('/password/forgot', {replace: true});
+
+
+    const onSubmit = async () => {
+        setIsLoading(true);
+
+        await logIn(dispatch, formData).then(() => {
+            if (token) {
+                formData.email = '';
+                formData.password = '';
+
+                navigate('/', {replace: true, state: {isLoggedIn: true}});
+            } else {
+                console.log('No token');
+            }
+            setIsLoading(false);
+        });
+    };
+
+    const handleInputChange = (e: { target: { name: string; value: string; }; }) => {
+        const {name, value} = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
 
     return (
         <div className={styles.loginContainer}>
-
-            <form className={styles.loginForm}>
+            <form className={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
                 <label className={styles.loginLabel}>
                     <p>{t("email")}</p>
-                    <input className={styles.loginInput} onChange={handleEmail} type="text"/>
+                    <input
+                        className={styles.loginInput}
+                        {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
+                        placeholder={t('email')}
+                        type="text"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        autoComplete={"email"}
+                    />
+                    {errors.email?.type === 'required' && <span className={styles.errorMsg}>{t('emailRequired')}</span>}
+                    {errors.email?.type === 'pattern' && <span className={styles.errorMsg}>Invalid email format</span>}
                 </label>
                 <label className={styles.loginLabel}>
                     <p>{t("password")}</p>
-                    <input className={styles.loginInput} onChange={handlePassword} type="password"/>
+                    <input
+                        className={styles.loginInput}
+                        {...register("password", {
+                            required: true,
+                            pattern: /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
+                        })}
+                        placeholder={t('password')}
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                        autoComplete={"current-password"}
+                    />
+
+                    {errors.password?.type === 'required' &&
+                        <span className={styles.errorMsg}>{t('passwordRequired')}</span>}
+                    {errors.password?.type === 'pattern' &&
+                        <span className={styles.errorMsg}>{t('passwordRequirement')}</span>}
+
                 </label>
-                <a onClick={goToForgotPassword}>Forgot Password</a>
+                <a className={styles.forgotPassword} onClick={goToForgotPassword}>Forgot Password</a>
                 <div>
-                    <button disabled={isLoading} onClick={handleSend} className={styles.loginSubmit}>{t("logIn")}</button>
+                    <button disabled={isLoading} className={styles.loginSubmit} type="submit">
+                        {t("logIn")}
+                    </button>
                 </div>
             </form>
         </div>
-
-
     );
 };
